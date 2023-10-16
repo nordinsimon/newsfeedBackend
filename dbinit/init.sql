71171ee48783802189de7564fbb5a2990c4bfda3
Oscar Wendt <oscar.wendt91@gmail.com>
2023-10-16 12:27:11 +0200

    changed password and compose file to work with dotenv

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -7,34 +7,33 @@ DROP TABLE IF EXISTS userRoles;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS article;
 
-
 CREATE TABLE users (
-  user_id varchar(36) PRIMARY KEY
-  username varchar(36) UNIQUE NOT NULL
-  email varchar(128) UNIQUE NOT NULL
-  password varchar(64) NOT NULL
-  created_at timestamp
+  user_id varchar(36) PRIMARY KEY,
+  username varchar(36) UNIQUE NOT NULL,
+  email varchar(128) UNIQUE NOT NULL,
+  password varchar(64) NOT NULL,
+  created_at timestamp,
   edited_at timestamp
 );
 
 CREATE TABLE userRoles (
-  userRoles_Id INT PRIMARY KEY
-  user_id varchar(36)
+  userRoles_Id INT PRIMARY KEY,
+  user_id varchar(36),
   role_id varchar(36)
 );
 
 CREATE TABLE roles (
-  role_id varchar(36) PRIMARY KEY
+  role_id varchar(36) PRIMARY KEY,
   role_name varchar(36) UNIQUE NOT NULL
 );
 
 CREATE TABLE article (
-  id varchar(36) PRIMARY KEY
-  title TEXT NOT NULL
-  link TEXT
-  content TEXT
-  user_id varchar(36)
-  created_at timestamp
+  id varchar(36) PRIMARY KEY,
+  title TEXT NOT NULL,
+  link TEXT,
+  content TEXT,
+  user_id varchar(36),
+  created_at timestamp,
   edited_at timestamp
 );
 
