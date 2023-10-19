0d6cbe9f878687c3e9ffe4825027f0200dd54642
Oscar Wendt <oscar.wendt91@gmail.com>
2023-10-19 15:49:35 +0200

    added changes to script to run only init

--- dbinit/run-always.sh
+++ dbinit/run-always.sh
@@ -4,10 +4,10 @@
 docker-entrypoint.sh mysqld &
 
 # Wait for MySQL to start up
-sleep 20
+sleep 10
 
 # Run your custom SQL script
-mysql -u root -p${MYSQL_ROOT_PASSWORD} < /docker-entrypoint-initdb.d/1_always.sql
+mysql -u root -p${MYSQL_ROOT_PASSWORD} < /docker-entrypoint-initdb.d/0_init.sql
 
 # Keep the container running
 tail -f /dev/null
