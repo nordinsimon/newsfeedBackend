57cef3440dec2afaa4b8a8fe96a1fa1c3e06d514
Oscar Wendt <oscar.wendt91@gmail.com>
2023-10-23 19:06:48 +0200

    need to change authentication middleware, added roles in db, refresh and access-token works with role

--- src/middleware/authentication.test.ts
+++ src/middleware/authentication.test.ts
@@ -5,7 +5,7 @@ import fetch from "node-fetch";
 dotenv.config();
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
-/* 
+
 test("should allow access for valid admin token", async ({ expect }) => {
   const token = ACCESS_TOKEN_SECRET;
 
@@ -19,7 +19,7 @@ test("should allow access for valid admin token", async ({ expect }) => {
 
   expect(res.status).toBe(200);
   expect(text).toBe("Admin Content");
-}); */
+});
 
 test.only("should deny access for invalid token", async ({ expect }) => {
   const res = await fetch("http://localhost:3000/api/news/access/create", {
