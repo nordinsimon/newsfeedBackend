a1934a6d5f50de8d65fcb4fc61467bf65f41008c
nordinsimon <simon.s.nordin@gmail.com>
2023-10-24 00:02:44 +0200

    Added routes Users get by Id

--- src/routes/routesUsers.ts
+++ src/routes/routesUsers.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { pool } from "../config/mysql.config";
 
 const router = express.Router();
 
-router.get("/getAll", async (_req, res) => {
+router.get("/getAll", async (_req, res: Response) => {
   const sqlQuery = "SELECT * FROM users";
   try {
     const connection = await pool.getConnection();
@@ -16,8 +16,34 @@ router.get("/getAll", async (_req, res) => {
   }
 });
 
-router.get("/getById", async (_req, res) => {
-  res.send("Get user by id");
+router.get("/getById", async (req: Request, res: Response) => {
+  const userId = req.body.id;
+
+  if (!userId) {
+    res.status(400).json({ error: "Missing user id" });
+    return;
+  }
+
+  const sqlQuery = "SELECT * FROM users WHERE user_id = ?";
+  try {
+    const connection = await pool.getConnection();
+    const [results] = await connection.query(sqlQuery, [userId]);
+    connection.release();
+
+    if (Array.isArray(results) && results.length > 0) {
+      const objResults = results[0];
+
+      if ("password" in objResults) {
+        delete objResults.password;
+      }
+      res.json(objResults);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.put("/update", async (_req, res) => {
